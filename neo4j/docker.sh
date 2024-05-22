093ce71f3e5b1b95c2c76eb9e793d80a5a4c3287
DjDavelafleur815 <david.dharma815@outlook.fr>
2024-05-22 15:12:32 +0200

    neo4j dockerization updated

--- neo4j/docker.sh
+++ neo4j/docker.sh
@@ -1,4 +1,8 @@
 docker run \
-    --publish=7474:7474 --publish=7687:7687 \
-    --volume=$HOME/neo4j/data:/data \
-    neo4j
+    -p 7474:7474 -p 7687:7687 \
+    --name neo4j-apoc \
+    -e NEO4J_apoc_export_file_enabled=true \
+    -e NEO4J_apoc_import_file_enabled=true \
+    -e NEO4J_apoc_import_file_use__neo4j__config=true \
+    -e NEO4J_PLUGINS=\[\"apoc\",\"apoc-extended\"\] \
+    neo4j:5.13.0
