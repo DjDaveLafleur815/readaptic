4f56c2a38e23b68bdeaefc3cb50a76b90d82f987
DjDaveLafleur815 <david.dharma815@outlook.fr>
2024-06-24 14:38:44 +0200

    Home Page Updated

--- app_interface/lib/views/HomeView.dart
+++ app_interface/lib/views/HomeView.dart
@@ -23,14 +23,42 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('READAPIC'),
+        title: const Text('READAPTIC',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          )),
         centerTitle: true,
       ),
-      body: views[currentIndex],
+      body: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 15),
+              Image.asset('assets/icon/playstore.png',
+                width: 150,
+                height: 150,
+              ),
+            Container(
+              margin: const EdgeInsets.only(top: 20), // Espace de 20 pixels en haut
+              child: Text(
+                'Welcome to Readaptic. This is a research project in home automation using an IoT system (Internet of Things) to connect MultiSensors via the Z-Wave protocol to a Home Assistant server',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: NavigationBar(
         selectedIndex: currentIndex,
         onDestinationSelected: (index) {
-          setState(() {});
+          setState(() {
+            currentIndex = index;
+          });
         },
         destinations: sensors,
       ),
--- app_interface/lib/views/tabs/sensor1View.dart
+++ app_interface/lib/views/tabs/sensor1View.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:readaptic_app_interface/globals/api.dart';
 import 'package:readaptic_app_interface/models/sensor.dart';
+import 'package:readaptic_app_interface/globals/sensorList.dart';
 
 class Sensor1View extends StatefulWidget {
   const Sensor1View({super.key});
@@ -11,11 +12,37 @@ class Sensor1View extends StatefulWidget {
 }
 
 class _Sensor1ViewState extends State<Sensor1View>  {
+  int currentIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Sensor 1"),
+      appBar: AppBar(
+        title: const Text('READAPIC',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          )
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 20), // Espace de 20 pixels en haut
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
